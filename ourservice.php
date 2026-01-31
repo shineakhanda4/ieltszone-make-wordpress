94e8e75b0b5b6771d485a8e54ba539f872b2f475
shineakhanda4 <shineakhanda49@gmail.com>
2026-01-31 18:43:06 +0000

    Refactor HTML to use WordPress functions for assets
    Updated title and linked styles and scripts dynamically using WordPress functions.

--- ourservice.php
+++ ourservice.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IELTS Zone Mym</title>
-    <link rel="stylesheet" href="style.css">
+    <title>IELTS Zone MYM</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-</head>
+    <?php wp_head(); ?> </head>
 <body>
 
     <header-component></header-component>
@@ -32,8 +32,8 @@
         </section>
     </main>
 
-    <footer-component></footer-component>
+ <footer-component></footer-component>
 
-    <script src="main.js" type="module"></script>
+    <?php wp_footer(); ?> <script src="<?php echo get_template_directory_uri(); ?>/main.js" type="module"></script>
 </body>
 </html>
