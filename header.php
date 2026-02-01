ceacda3040d373cfc03e23982aa9efcc9a665473
shineakhanda4 <shineakhanda49@gmail.com>
2026-02-01 01:50:31 +0000

    Update header.php

--- header.php
+++ header.php
@@ -3,9 +3,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
     <header-component></header-component>
